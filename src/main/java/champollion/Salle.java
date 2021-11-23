b40d97df7876d82533eff45b9467a6bff18118fc
chabus <chabus@10.157.1.112>
2021-11-23 08:53:30 +0100

    Création classe Salle

---
+++ src/main/java/champollion/Salle.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package champollion;
+
+/**
+ *
+ * @author chabus
+ */
+public class Salle {
+    private String intitulé;
+    private int capacité;
+
+    //constructeur n°1
+    public Salle(String intitulé, int capacité) {
+        this.intitulé = intitulé;
+        this.capacité = capacité;
+    }
+    
+    
+}
