cb44076a8098af82ca1eae3fa1818ca0e96595af
chabus <chabus@10.157.1.112>
2021-11-23 09:16:52 +0100

    Ajout classe intervention + Constructeur n°2 et attributs de la classe UE

--- src/main/java/champollion/UE.java
+++ src/main/java/champollion/UE.java
@@ -2,10 +2,23 @@ package champollion;
 
 public class UE {
     private final String myIntitule;
+    public int heuresCM;
+    public int heuresTD;
+    public int heuresTP;
 
+    //constructeur n°1
     public UE(String intitule) {
         myIntitule = intitule;
     }
+
+    //constructeur n°2
+    public UE(String myIntitule, int heuresCM, int heuresTD, int heuresTP) {
+        this.myIntitule = myIntitule;
+        this.heuresCM = heuresCM;
+        this.heuresTD = heuresTD;
+        this.heuresTP = heuresTP;
+    }
+    
 
     public String getIntitule() {
         return myIntitule;
